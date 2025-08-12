cd99583dab6a8024f7bf9858ff312fe9d8d0b81d
jordypx <jordanodo81@gmail.com>
2025-08-12 16:44:15 -0600

    porfolio

---
+++ src/constants/index.js
@@ -0,0 +1,168 @@
+import { mobile, backend, web } from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Designer",
+    icon: web,
+  },
+  {
+    title: "Video Editor",
+    icon: mobile,
+  },
+  {
+    title: "Seo Specialist & Ads Expert",
+    icon: backend,
+  },
+];
+
+const projects = [
+  {
+    name: "AudioPhile",
+    description:
+      "An e-commerce website for a purchasing audio devices that are available.",
+    tags: [
+      {
+        name: "Astro.js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "GSAP",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: "src/assets/audiophile.png",
+    source_code_link: "https://github.com/Jordypx/Audiophile_webpage",
+    site_url: "https://audiophile-ecommerce-mbart13.vercel.app/",
+  },
+  {
+    name: "PhotoSnap",
+    description:
+      "A marketing website for a photo-sharing application and photo display.",
+    tags: [
+      {
+        name: "reactjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "flexbox",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: "src/assets/photosnap.png",
+    source_code_link: "https://github.com/Jordypx/photosnapWeb",
+    site_url: "https://photosnap-web-gold.vercel.app",
+  },
+  {
+    name: "Scoot website",
+    description:
+      "Scoot is a platform that takes the hassle out of urban mobility for the people.",
+    tags: [
+      {
+        name: "Astro.js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "motion",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: "src/assets/scoot.png",
+    source_code_link: "https://github.com/Jordypx/scoot-website",
+    site_url: "https://scoot-website-delta.vercel.app",
+  },
+  {
+    name: "Dinner",
+    description:
+      "Dinner is a platform where you can book and manage your appointments to eat at the restaurant",
+    tags: [
+      {
+        name: "reactjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "framer-motion",
+        color: "green-text-gradient",
+      },
+      {
+        name: "vite",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: "src/assets/dinner.png",
+    source_code_link: "https://github.com/Jordypx/dinner",
+    site_url: "https://dinner-sand.vercel.app",
+  },
+  {
+    name: "Arch",
+    description:
+      "It's a platform that showcases visual display and helps bring projects to life with aim of meeting the clients need.",
+    tags: [
+      {
+        name: "reactjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "framer-motion",
+        color: "green-text-gradient",
+      },
+      {
+        name: "vite",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: "src/assets/arch.png",
+    source_code_link: "https://github.com/Jordypx/Arch",
+    site_url: "https://archive-ebon.vercel.app",
+  },
+  {
+    name: "Designo",
+    description:
+      "It's a platform that showcases visual display and helps bring projects to life with aim of meeting the clients need.",
+    tags: [
+      {
+        name: "reactjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "framer-motion",
+        color: "green-text-gradient",
+      },
+      {
+        name: "vite",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: "src/assets/designo.png",
+    source_code_link: "https://github.com/Jordypx/designo_page",
+    site_url: "https://designo-page.vercel.app",
+  },
+];
+
+export { services, projects };
